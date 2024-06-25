31225886ae2ba42aca872a558c5d6649e03833c8
Sila <rsilagenc@hotmail.com>
2024-06-25 10:29:04 +0300

    Revert "Initial Commit"
    This reverts commit fc6d375c8073e3d16c13b6259158b5c58b136d73.

--- testClient.py
+++
@@ -1,7 +0,0 @@
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("127.0.0.1", 8080))
-data = b"012345"
-s.sendall(data)
-#print("---- Received ----\n", s.recv(4096))
-s.close()
